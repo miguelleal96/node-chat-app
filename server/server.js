0a5542e71bde6257b0bcb5a52a87e53076516b4c
miguelleal96 <miguelleal96@gmail.com>
2017-09-10 21:34:51 -0500

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -17,19 +17,39 @@ io.on('connection', (socket) => {
   /*
     socket.emit - emits event to a single connection
     io.emit - emits event to every single connection
+
+    Broadcasting - emitting an event, but one specific user
   */
+  // emitts event to an individual user who joins
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  })
+  // emitts event to all other users connected, except the user who joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New User Joined',
+    createdAt: new Date().getTime()
+  })
   console.log('new user connected')
 
   // handles created message FROM the client
   socket.on('createMessage', (message) => {
-    //console.log('created a message from the client', message)
-
+    // console.log('created a message from the client', message)
     // Sends a message TO the client
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     })
+
+    // send event to everybody else, but this socket
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   created: new Date().getTime()
+    // })
   })
 
   socket.on('disconnect', () => {
